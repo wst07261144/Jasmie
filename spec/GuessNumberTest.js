5eb199f7ec9fb4ed04298bf6993eab256435bb58
wst07261144 <wst07261144@gmail.com>
2013-09-13 16:52:51 +0800

    123

---
+++ spec/GuessNumberTest.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: wst
+ * Date: 13-9-13
+ * Time: 上午9:58
+ * To change this template use File | Settings | File Templates.
+ */
+describe("test add",function()
+{
+    it("should be 3",function(){
+        var a = add();
+        expect(a).toBe(3)
+    }) ;
+
+});
---
+++ src/GuessNumber.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: wst
+ * Date: 13-9-13
+ * Time: 上午9:57
+ * To change this template use File | Settings | File Templates.
+ */
+function add()
+{
+    return 1+2;
+}
